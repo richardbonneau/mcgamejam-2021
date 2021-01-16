6881bc4cbc52a6bc890b589e9cbb38206130a812
richardbonneau <36018812+richardbonneau@users.noreply.github.com>
2021-01-16 13:27:22 -0500

    cell door works

--- containment-cell/scripts/celldoor.gd
+++ containment-cell/scripts/celldoor.gd
@@ -1,14 +1,26 @@
 extends KinematicBody
 
 onready var door_opened = false
+onready var trigger_open_door = false
+
+var destination
+var offset
 
 func trigger_door():
-	if door_opened:
-		rotation.y = atan2(0, -0.7)
-		door_opened = false
-	else:
-		rotation.y = atan2(0, 0.7)
-		door_opened = true
+	print("celldoor")
+	if !door_opened:
+		var self_origin = get_global_transform().origin
+		destination = Vector3(self_origin.x,self_origin.y,self_origin.z+2)
+		
+		trigger_open_door = true
+
 	
-	
+func _physics_process(delta):
+	if trigger_open_door:
+		var self_origin = get_global_transform().origin
+		offset = self_origin - destination
+		var distance_to_dest = offset.length()
+		print("distance_to_dest ",distance_to_dest)
+		move_and_slide(offset.normalized() * 45 * delta)
+		if distance_to_dest > 6.5: trigger_open_door = false
 	
