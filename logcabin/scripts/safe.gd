f8f9c35ec2179bef8d86da18f9501ae1f2bbe4a7
richardbonneau <36018812+richardbonneau@users.noreply.github.com>
2021-01-17 12:58:32 -0500

    last bug fixes

--- logcabin/scripts/safe.gd
+++ logcabin/scripts/safe.gd
@@ -9,18 +9,6 @@ func open_door():
 	elif !door_opened:
 		rotation.y = atan2(-0.5, 0)
 		door_opened = true
-
-
-#func trigger_door():
-#	if self.get_owner().is_neighbour_house and !main.player_inventory.neighbour_house_key:
-#		main.get_node("DialogueBox").talk(main.player_messages.door_locked,false)
-#		return
-#
-#	if !door_opened:
-#		rotation.y = atan2(0, -0.7)
-#		door_opened = true
-#	else:
-#		rotation.y = atan2(0, 0.7)
-#		door_opened = false
-#
-	
+		$CollisionShape.queue_free()
+		$CollisionShape2.queue_free()
+		$CollisionShape3.queue_free()
